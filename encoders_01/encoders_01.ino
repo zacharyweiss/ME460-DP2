90cb2650e8c39112fd52f1873b871dc63527be62
Zachary Weiss <zachary.weiss.acct@gmail.com>
2020-10-23 00:07:02 -0400

    Update encoders_01.ino

--- encoders_01/encoders_01.ino
+++ encoders_01/encoders_01.ino
@@ -1,26 +1,33 @@
 /* Zachary Weiss
 ** 23 Oct 2020
 ** Linear Rail DC Closed-Loop Control fo ME460 */
-#define PITCH 8; // mm per rev
+#define PITCH     8 // mm per rev
+#define PULSES  374 // pulses per rev
 
 // global settings
-// k is coefficient for proportional control
-double k          = 0.05; 
-int    targetRPM1 = 35, // mm per s
-       targetRPM2 = 20,
-       targetRPM3 = 10, // slowest speed possible
-       targetRPM4 = 10,
-       dist2      = 10, // mm
-       dist4      = 30;
+// k is coeff for proportional term
+// c is coeff for derivative term
+double k          = 0.1,
+       c          = 0.1;
+int    targetSpd1 = 35, // mm per s
+       targetSpd2 = 20,
+       targetSpd3 = 10, // slowest speed possible
+       targetSpd4 = 10,
+       dist2      = 20, // mm
+       dist4      = 60;
 
-// global vars
+// global vars and flags
 volatile long int pulses = 0;
 double v_motor           = 0, 
        current_rpm       = 0,
-       dist_ct           = 0,
-       err               = 0;
+       err               = 0,
+       err_old           = 0,
+       d_err             = 0,
+       use_derivative    = 0; // flag
 
 void setup() {
+    Serial.begin(115200);
+    
     attachInterrupt(digitalPinToInterrupt(2),count,RISING);
     
     pinMode(12,OUTPUT); // Dir
@@ -35,43 +42,40 @@ void setup() {
     digitalWrite(3,LOW); // Motor off
     digitalWrite(9,LOW); // Disable break
     digitalWrite(12,HIGH);  // Set direction
-    Serial.begin(115200);
 
     while(digitalRead(10)); // wait for user input
     delay(1000); // wait one second before beginning sequence
 }
 
 void loop() {
+  int ct0 = 0;
 
-  // Output voltage to motor, wait for response, measure new speed
-  analogWrite(3,int(v_motor));
-  current_rpm=getSpeed(10000); 
-
-  // Print time [mcs], voltage [0-255], speed [rpm]
-  Serial.println(String(millis())+"\t"+String(int(v_motor))+"\t"+String(current_rpm));
-
-  err = target_rpm - current_rpm;
-  v_motor += k*err;
-
-  v_motor = (v_motor>255)?255:((v_motor<0)?0:v_motor);
-  delay(100);
-
-  /*
-  t0=micros();
-  ct=0;
-  while(micros()-t0<1E+6){
-    v2=digitalRead(7);
-    if (v2>v1)
-      ct++;
-    v1=v2;
+  // towards motor
+  digitalWrite(12,HIGH);
+  while(digitalRead(10)) {
+    setSpeed(mmToRPM(targetSpd1));
   }
-  w=ct/12*60;  // Cycles per minute, or r.p.m.         
- 
-  Serial.println(w);
-  analogWrite(3,0);
-  digitalWrite(9,HIGH);
-  while(true);*/
 
+  // away from motor
+  ct0 = pulses;
+  digitalWrite(12,LOW);
+  while((ct0-pulses)<(dist2*PULSES/PITCH)) {
+    setSpeed(mmToRPM(targetSpd2));
+  }
+  
+  digitalWrite(12,HIGH);
+  while(digitalRead(10)) {
+    setSpeed(mmToRPM(targetSpd3));
+  }
+  
+  ct0 = pulses;
+  digitalWrite(12,LOW);
+  while((ct0-pulses)<(dist4*PULSES/PITCH)) {
+    setSpeed(mmToRPM(targetSpd4));
+  }
+
+  shutdown();
+  while(digitalRead(10)); // hold until reset
 }
 
 void count() {
@@ -82,22 +86,58 @@ void count() {
     pulses++;
 }
 
-double getSpeed(long int mcs) {
-  unsigned long int t0=0, initialPulses=0, delta_pulses=0;
-  double rpm=0;
-  
-  initialPulses=pulses;
-  t0=micros();
-  while(micros()-t0<mcs){} // hold for sample time
-  delta_pulses = pulses - initialPulses;
+void setSpeed(double target_rpm) {
+  // Output voltage to motor, wait for response, measure new speed
+  analogWrite(3,int(v_motor));
+  delay(100);
+  current_rpm = getSpeed(100000); 
 
-  rpm=double(delta_pulses)/double(mcs)*1E+6 /374.0 * 60.0; // 374 pulses/rev, 60 seconds per min
+  // Print time [mcs], voltage [0-255], speed [rpm], pulses
+  Serial.println(String(millis())
+    +"\t"
+    +String(int(v_motor))
+    +"\t"
+    +String(current_rpm)
+    +"\t"
+    +String(pulses));
+
+  err = target_rpm - current_rpm;
+  // only calculate d_err if use_derivative is 1, else 0
+  d_err = use_derivative?(err - err_old):0;
+  v_motor += k*err + c*d_err;
+  err_old = err;
+
+  // cap voltage between 0 and 255
+  v_motor = int((v_motor>255)?255:((v_motor<0)?0:v_motor));
+}
+
+double getSpeed(long int mcs) {
+  unsigned long int t0           = 0,
+                    delta_pulses = 0;
+  // should never be neg, but to do math where the
+  // result is neg, must make signed else result will
+  // overflow, despite abs().
+  long int initialPulses         = 0;
+  double rpm                     = 0;
+  
+  initialPulses = pulses;
+  t0 = micros();
+  while(micros()-t0<mcs){} // hold for sample time
+  delta_pulses = abs(initialPulses-pulses);
+
+  rpm = double(delta_pulses)/double(mcs)*1E+6 /double(PULSES) * 60.0;
 
   return(rpm);
 }
 
 double mmToRPM(int mmPerS) {
   // converts mm/s to RPM
-  double rpm = mmPerS*60/PITCH
-  return rpm
+  double rpm = mmPerS*60/PITCH;
+  return(rpm);
+}
+
+void shutdown() {
+  digitalWrite(3,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(12,LOW);
 }
